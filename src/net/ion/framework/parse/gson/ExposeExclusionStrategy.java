51e89d3f037fd98eeef9277d7aed1bed060e81ed
bleujin <bleujin@gmail.com>
2012-11-09 14:43:29 +0900

    ..

---
+++ src/net/ion/framework/parse/gson/ExposeExclusionStrategy.java
@@ -0,0 +1,18 @@
+package net.ion.framework.parse.gson;
+
+import net.ion.framework.parse.gson.annotations.Expose;
+
+public class ExposeExclusionStrategy implements ExclusionStrategy{
+
+	public boolean shouldSkipClass(Class<?> clazz) {
+		return false;
+	}
+
+	public boolean shouldSkipField(FieldAttributes field) {
+		Expose annotation = field.getAnnotation(Expose.class);
+		if (annotation == null) return false ;
+		
+		return annotation.serialize() ;
+	}
+
+}
