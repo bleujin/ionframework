bf1c5c5a2d46985a81114a9578adfb5823887350
bleujin@gmail.com <bleujin@gmail.com>
2016-10-10 16:14:41 +0900

    lastverserion with jdk5

--- test/net/ion/framework/db/postgre/TestPostgreSqlManager.java
+++ test/net/ion/framework/db/postgre/TestPostgreSqlManager.java
@@ -1,12 +1,16 @@
 package net.ion.framework.db.postgre;
 
 
+import java.sql.ResultSetMetaData;
+
 import junit.framework.TestCase;
 import net.ion.framework.db.DBController;
 import net.ion.framework.db.Rows;
 import net.ion.framework.db.manager.DBManager;
+import net.ion.framework.db.manager.OracleDBManager;
 import net.ion.framework.db.manager.PostSqlDBManager;
 import net.ion.framework.db.servant.StdOutServant;
+import net.ion.framework.util.Debug;
 
 public class TestPostgreSqlManager extends TestCase {
 
@@ -21,7 +25,7 @@ public class TestPostgreSqlManager extends TestCase {
 
     protected void setUp() throws java.lang.Exception {
         super.setUp();
-        DBManager dbm = new PostSqlDBManager("jdbc:postgresql://127.0.0.1:5432/ics6copy", "postgres", "redf") ;
+        DBManager dbm = new PostSqlDBManager("jdbc:postgresql://127.0.0.1:5432/ics6", "postgres", "redf") ;
         dc = new DBController("Default", dbm) ;
         dc.addServant(new StdOutServant()) ;
         dc.initSelf() ;
@@ -32,6 +36,16 @@ public class TestPostgreSqlManager extends TestCase {
         dc.destroySelf() ;
     }
 
+    
+	public void testColumnLabel() throws Exception {
+		
+		Rows rows = dc.createUserCommand("select 1 a").execQuery() ;
+		ResultSetMetaData meta = rows.getMetaData() ;
+		
+		for(int i = 1 ; i <= meta.getColumnCount(); i++){
+			Debug.line(meta.getColumnLabel(i), meta.getColumnName(i)) ;
+		}
+	}
 
     public void testDBManager() throws Exception{
     	Rows rows = dc.getRows("select * from sample_tbl ");
